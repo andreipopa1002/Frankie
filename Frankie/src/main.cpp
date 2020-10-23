1f3e8ef54375cc9ecb045e24a4a40970e5dfb6d9
Andrei Popa <andrei.popa@tandem.co.uk>
2020-10-23 11:29:14 +0100

    Added temperature switching

--- Frankie/src/main.cpp
+++ Frankie/src/main.cpp
@@ -8,17 +8,36 @@ void setup() {
   Serial.begin(9600);
   while(!Serial);
 
-  thermo = new TemperatureSensor(D1, D2, D5, D6);
+  thermo = new TemperatureSensor(D3, D4, D5, D6);
   Serial.println();
   Serial.println();
   Serial.println("Adafruit MAX31865 PT100 Sensor Test!");
   thermo->begin();
+
+  pinMode(D1, OUTPUT);
+  pinMode(D0, OUTPUT);
+  digitalWrite(D1,LOW);
+  digitalWrite(D0,LOW);
 }
 
 
 void loop() {
-  Serial.print("temperature: ");
-  Serial.println(thermo->temperature());
+  float temperature = thermo->temperature();
+  Serial.print("temperature:");
+  Serial.println(temperature);
+  
+
+  if (temperature > 25) {
+    digitalWrite(D0, HIGH);
+  } else {
+    digitalWrite(D0, LOW);
+  }  
+
+  if (temperature >27) {
+    digitalWrite(D1, HIGH);
+  } else {
+    digitalWrite(D1, LOW);
+  }
 
   delay(1000);
 }
