14102a600a7eb8426657ca1e4dfd1314601d69ce
Andrei Popa <andrei.popa@tandem.co.uk>
2020-10-29 21:46:47 +0000

    Added read only state for the switches

--- Frankie/src/Relay/Relay.cpp
+++ Frankie/src/Relay/Relay.cpp
@@ -1,22 +1,41 @@
 #include "Arduino.h"
 #include "Relay.h"
 
-Relay::Relay(int pinSwitch1, int pinSwitch2) {
+Relay::Relay(
+    int pinSwitch1,
+    int pinSwitch2,
+    bool isSwitch1Active, 
+    bool isSwitch2Active
+) {
     _pinSwitch1 = pinSwitch1;
     _pinSwitch2 = pinSwitch2;
+    _isSwitch1Active = isSwitch1Active;
+    _isSwitch2Active = isSwitch2Active;
 }
 
 void Relay::begin() {
     pinMode(_pinSwitch1, OUTPUT);
     pinMode(_pinSwitch2, OUTPUT);
-    activateSwitch1(false);
-    activateSwitch2(false);
+    activateSwitch1(_isSwitch1Active);
+    activateSwitch2(_isSwitch2Active);
 }
 
-void Relay::activateSwitch1(bool isOn) {
-    digitalWrite(_pinSwitch1, isOn ? HIGH : LOW);
+bool Relay::isSwitch1Active() {
+    return _isSwitch1Active;
 }
 
-void Relay::activateSwitch2(bool isOn) {
-    digitalWrite(_pinSwitch2, isOn ? HIGH : LOW);
+bool Relay::isSwitch2Active() {
+    return _isSwitch2Active;
+}
+
+void Relay::activateSwitch1(bool isActive) {
+    if (_isSwitch1Active != isActive) {
+        digitalWrite(_pinSwitch1, isActive ? HIGH : LOW);
+    }
+}
+
+void Relay::activateSwitch2(bool isActive) {
+    if (_isSwitch2Active != isActive) {
+        digitalWrite(_pinSwitch2, isActive ? HIGH : LOW);
+    }
 }
--- Frankie/src/Relay/Relay.h
+++ Frankie/src/Relay/Relay.h
@@ -5,14 +5,23 @@
 
 class Relay {
     public:
-        Relay(int pinSwitch1, int pinSwitch2);
+        Relay(
+            int pinSwitch1,
+            int pinSwitch2,
+            bool isSwitch1Active = false,
+            bool isSwitch2Active = false
+        );
         void begin();
-        void activateSwitch1(bool isOn);
-        void activateSwitch2(bool isOn);
+        void activateSwitch1(bool isActive);
+        void activateSwitch2(bool isActive);
+        bool isSwitch1Active();
+        bool isSwitch2Active();
         
     private:
         int _pinSwitch1;
         int _pinSwitch2;
+        bool _isSwitch1Active;
+        bool _isSwitch2Active;
 };
 
 #endif
